51eb330741b782ce2c66651e6c8d0839e08d6020
Mario Román Dono <mario.romandono@gmail.com>
2021-12-08 23:53:50 +0100

    Añadir licencia

--- lanzarContainers.sh
+++ lanzarContainers.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# Copyright (C) 2021 Mario Román Dono
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 # Uso: ./lanzarContainers.sh n_redes [<nombre:imagen:redes>]
 
 # El contenedor se arranca en modo detached para poder saber cuándo termina de crearse y poder configurar las redes correctamente

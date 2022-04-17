f0b17af00dbf10be02cb2c3ebd6829eaa6feb6f3
Mario Román Dono <mario.romandono@gmail.com>
2022-04-17 19:44:49 +0000

    Add creation of terminal windows in create option

--- cmd/LabsOnContainers/create.go
+++ cmd/LabsOnContainers/create.go
@@ -12,6 +12,8 @@ import (
 // createLabEnviroment parses a YAML file and, if it has a correct format,
 // it converts each element of the file and calls the LabsOnContainers API.
 func createLabEnviroment(filePath string) {
+    fmt.Println("Creando el entorno de laboratorio...")
+
     file, err := os.ReadFile(filePath)
     if err != nil {
         fmt.Printf("error while opening file: %v\n", err)
@@ -31,6 +33,22 @@ func createLabEnviroment(filePath string) {
         fmt.Printf("error on labsoncontainers: %v\n", err)
         os.Exit(1)
     }
-    
-    createTerminalWindows(containersIds)
+
+    var foregroundContainersIds []string
+    for _, container := range labEnv.Containers {
+        if !container.Background {
+            foregroundContainersIds = append(foregroundContainersIds, containersIds[container.Name])
+        }
+    }    
+    err = createTerminalWindows(foregroundContainersIds) 
+    if err != nil {
+        fmt.Printf("error while creating terminal windows: %v\n", err)
+        destroyErr := labsoncontainers.DestroyEnviroment(labEnv.LabName)
+        if destroyErr != nil {
+            fmt.Printf("error on labsoncontainers: %v\n", err)
+        }
+        os.Exit(1)
+    }
+
+    fmt.Println("Entorno creado exitosamente")
 }
--- cmd/LabsOnContainers/utils.go
+++ cmd/LabsOnContainers/utils.go
@@ -1,5 +1,37 @@
 package main
 
-func createTerminalWindows(containersIds []string) {
-	// TODO
+import (
+	"os/exec"
+	"os"
+	"syscall"
+)
+
+// createTerminalWindows creates terminal windows that make possible the
+// interaction with the created containers. At the moment, this function
+// only works with XFCE terminal, and it should be refactored in order to 
+// support more terminal emulators.
+func createTerminalWindows(containersIds []string) error {
+	args := []string{"-e"}
+	for i := 0; i < len(containersIds); i++ {
+		if i > 0 {
+			args = append(args, "--tab", "-e")
+		}
+		args = append(args, "ash -c 'docker container attach " + containersIds[i] + "; exec ash'")
+	}
+
+	// GTK apps like xfce4-terminal won't run in setuid processes, so it is necessary
+	// to create the process using the real UID and GID
+	cmd := exec.Command("xfce4-terminal", args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Credential: &syscall.Credential{
+			Uid: uint32(os.Getuid()), 
+			Gid: uint32(os.Getgid()),
+		},
+	}
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
